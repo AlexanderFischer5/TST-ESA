57628a1da73d04aafc79a4d7c4213cdb6bb11ce9
AlexanderFischer5 <alexander.fischer55@web.de>
2026-06-26 23:12:34 +0200

    GREEN: getQuantity implementiert

--- src/shoppingcart/ShoppingCart.java
+++ src/shoppingcart/ShoppingCart.java
@@ -3,12 +3,19 @@ package shoppingcart;
 public class ShoppingCart {
 
     private double gesamtpreis;
+    private int anzahl;
 
     public void addArtikel(double preis) {
         gesamtpreis += preis;
+        anzahl++;
     }
 
     public double getGesamtpreis() {
         return gesamtpreis;
     }
+
+    public int getQuantity() {
+        return anzahl;
+    }
+
 }
